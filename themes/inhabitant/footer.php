130d23cec069c5ccd41fa16704e942d048dff595
Eric Chan <ericchan277@gmail.com>
2018-02-23 09:32:06 -0800

    Moved footer sidebar code so that it's properly in the footer

--- themes/inhabitant/footer.php
+++ themes/inhabitant/footer.php
@@ -11,38 +11,40 @@
 
 			<footer id="colophon" class="site-footer" role="contentinfo">
 			<i class="fab fa-accessible-icon"></i>
-				<div class="site-info">
+				<div class="site-info">	<div id="footer-sidebar" class="secondary">
+				<div id="footer-sidebar1">
+				<?php
+				if(is_active_sidebar('footer-sidebar-1')){
+				dynamic_sidebar('footer-sidebar-1');
+				}
+				?>
+				</div>
+				<div id="footer-sidebar2">
+				<?php
+				if(is_active_sidebar('footer-sidebar-2')){
+				dynamic_sidebar('footer-sidebar-2');
+				}
+				?>
+				</div>
+				<div id="footer-sidebar3">
+				<?php
+				if(is_active_sidebar('footer-sidebar-3')){
+				dynamic_sidebar('footer-sidebar-3');
+				}
+				?>
+				</div>
+				</div>
+					
 
 					<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s ' ), 'WordPress' ); ?></a>
 				</div><!-- .site-info -->
 			</footer><!-- #colophon -->
 		</div><!-- #page -->
 
+
 		<?php wp_footer(); ?>
 
 	</body>
 
-	<div id="footer-sidebar" class="secondary">
-<div id="footer-sidebar1">
-<?php
-if(is_active_sidebar('footer-sidebar-1')){
-dynamic_sidebar('footer-sidebar-1');
-}
-?>
-</div>
-<div id="footer-sidebar2">
-<?php
-if(is_active_sidebar('footer-sidebar-2')){
-dynamic_sidebar('footer-sidebar-2');
-}
-?>
-</div>
-<div id="footer-sidebar3">
-<?php
-if(is_active_sidebar('footer-sidebar-3')){
-dynamic_sidebar('footer-sidebar-3');
-}
-?>
-</div>
-</div>
+
 </html>
