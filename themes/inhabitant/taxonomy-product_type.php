efe2745b502b3bcc154d5951a79ff8c003edad2d
Eric Chan <ericchan277@gmail.com>
2018-03-06 10:04:32 -0800

    completed layout for taxonomy-product_type

--- themes/inhabitant/taxonomy-product_type.php
+++ themes/inhabitant/taxonomy-product_type.php
@@ -29,7 +29,17 @@ get_header(); ?>
 			<?php the_post_thumbnail( 'small' ); ?>
 		<?php endif; ?>
 
-		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+						<?php $price =  CFS()->get( 'price' );?>
+					<div class="product-meta-box">
+
+
+						<?php the_title('<p class="prod-title">', '</p>') ?>
+
+						<?php echo '<span class="price">'. $price . '</span>'?> 
+					
+		
+					</div>
+						<p class="elipses">.........................................</p>
 
 		<?php if ( 'post' === get_post_type() ) : ?>
 		<div class="entry-meta">
