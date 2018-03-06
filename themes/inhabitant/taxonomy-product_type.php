668ebe76832d512cbb57cafb1623248ca8de9d18
Eric Chan <ericchan277@gmail.com>
2018-03-06 09:52:03 -0800

    Added basic flexbox layout to taxonomy-product_type

--- themes/inhabitant/taxonomy-product_type.php
+++ themes/inhabitant/taxonomy-product_type.php
@@ -20,14 +20,32 @@ get_header(); ?>
 			</header><!-- .page-header -->
 
 			<?php /* Start the Loop */ ?>
+			<div class="content-wrapper">
 			<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php
-					get_template_part( 'template-parts/content' );
-				?>
+				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<?php if ( has_post_thumbnail() ) : ?>
+			<?php the_post_thumbnail( 'small' ); ?>
+		<?php endif; ?>
 
+		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+
+		<?php if ( 'post' === get_post_type() ) : ?>
+		<div class="entry-meta">
+			<?php red_starter_posted_on(); ?> / <?php comments_number( '0 Comments', '1 Comment', '% Comments' ); ?> / <?php red_starter_posted_by(); ?>
+		</div><!-- .entry-meta -->
+		<?php endif; ?>
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+		<?php the_excerpt(); ?>
+	</div><!-- .entry-content -->
+</article><!-- #post-## -->
+
+		
 			<?php endwhile; ?>
-
+			</div>
 			<?php the_posts_navigation(); ?>
 
 		<?php else : ?>
@@ -39,5 +57,4 @@ get_header(); ?>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
